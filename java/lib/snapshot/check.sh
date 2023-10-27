0eeb71286064aced81f00d11a8bc2327dbb9b191
Stanley Wintergreen <kepocnhh@gmail.com>
2023-10-28 00:43:42 +0300

    fix task code quality

--- java/lib/snapshot/check.sh
+++ java/lib/snapshot/check.sh
@@ -3,14 +3,14 @@
 VARIANT='snapshot'
 
 gradle clean && \
- gradle "checkLicense" && \
- gradle "checkCodeStyle" && \
+ gradle 'checkLicense' && \
+ gradle 'checkCodeStyle' && \
  gradle "lib:check${VARIANT^}Readme" && \
- gradle "lib:checkUnitTest" && \
- gradle "lib:checkCoverage" && \
- gradle "lib:checkCodeQualityMain" && \
- gradle "lib:checkCodeQualityTest" && \
- gradle "lib:checkDocumentation"
+ gradle 'lib:checkUnitTest' && \
+ gradle 'lib:checkCoverage' && \
+ gradle 'lib:checkCodeQuality' && \
+ gradle 'lib:checkCodeQualityTest' && \
+ gradle 'lib:checkDocumentation'
 
 if test $? -ne 0; then
  echo 'Check error!'; exit 1; fi
